9e229d4da01d9a76016d77adfe397c391a88cd23
Kishor Suryabhan Handge <kishorhandge29@gmail.com>
2026-08-09 22:22:31 +0530

    Add files via upload

---
+++ System_Call/Mutex/Mutex2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<pthread.h>
+
+pthread_mutex_t lock;
+
+int iCnt = 0;
+
+void * Increment(void * arg)
+{   
+        //critical section start
+    pthread_mutex_lock(&lock);
+        iCnt++;
+        //critical section end
+    
+        printf("%d\n",iCnt);
+
+    pthread_mutex_unlock(&lock);
+
+    return NULL;
+}
+int main()
+{   
+    pthread_t t1,t2;
+
+    pthread_mutex_init(&lock,NULL);
+
+    pthread_create(&t1,NULL,Increment ,NULL);
+    pthread_create(&t2,NULL,Increment ,NULL);
+
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+
+    pthread_mutex_destroy(&lock);
+
+    return 0;
+}
